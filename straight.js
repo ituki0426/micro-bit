cddebc75b28131073f2de4aa66245c436d2855df
ituki <82156802+ituki0426@users.noreply.github.com>
2022-10-06 12:27:11 +0900

    Update straight.js

--- straight.js
+++ straight.js
@@ -2,6 +2,10 @@ input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 0)
 })
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
